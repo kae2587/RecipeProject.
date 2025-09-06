e19e05355dffdf01268fabf1c14236f9568106bf
Kaylee Marquez <marquezkaylee50@gmail.com>
2025-09-06 14:28:08 -0700

    Fix CORS to allow Firebase domain

--- Backend/app.js
+++ Backend/app.js
@@ -633,10 +633,19 @@ require('dotenv').config();
 app.use(express.static(path.join(__dirname, '../Frontend/public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+// app.use(cors({
+//   origin: 'http://localhost:3000',
+//   credentials: true
+// }));
+
+
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: "https://recipeprojecttwo.web.app",  // your Firebase Hosting URL
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
+
+
 app.use(express.static(path.join(__dirname, '../Frontend/build')));
 
 // Route to test frontend serving
